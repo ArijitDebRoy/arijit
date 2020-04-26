a406ccdb9978fba9123fbf8cfa73298772a0d03a
Arijit Deb Roy <arijitdebroy24@gmail.com>
2020-04-26 16:55:30 +0530

    favicon change and postcss purger added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-const withPWA = require('next-pwa')
+const withPWA = require('next-pwa');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = withPWA({
+    localIdentName: "[local]__[hash:base64:5]",
     pwa: {
         dest: 'public'
+    },
+    crossOrigin: 'anonymous',
+    webpack: (config, options) =>
+    {
+        config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+        config.optimization.minimizer.push(new UglifyJsPlugin());
+        return config;
     }
 })
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,21 @@
 module.exports = {
     plugins: [
         'tailwindcss',
+        ...(
+            [
+                [
+                    '@fullhuman/postcss-purgecss',
+                    {
+                        content: [
+                            './pages/**/*.{js,jsx,ts,tsx}',
+                            './components/**/*.{js,jsx,ts,tsx}',
+                        ],
+                        defaultExtractor: content =>
+                            content.match(/[\w-/:]+(?<!:)/g) || [],
+                    },
+                ],
+            ]
+        ),
         'postcss-preset-env'
     ]
 }
