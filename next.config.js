2c9011d0fb8a9556ed589dd1d5ef4c38da3f5fe2
Arijit Deb Roy <arijitdebroy24@gmail.com>
2020-04-26 14:18:09 +0530

    next.config change for pwa

--- next.config.js
+++ next.config.js
@@ -1,33 +1,7 @@
-const withPWA = require('next-pwa');
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const withPurgeCss = require('next-purgecss');
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const withPWA = require('next-pwa')
 
-module.exports = withPWA(withPurgeCss(
-    {
-        purgeCssPaths: [
-            'pages/**/*',
-            'components/**/*',
-            'layouts/**/*'
-        ],
-        purgeCss: {
-            whitelist: () => [],
-            whitelistPatterns: () => [/^Toastify/]
-        }
-    }
-), {
-    localIdentName: "[local]__[hash:base64:5]",
+module.exports = withPWA({
     pwa: {
-        dest: 'public',
-        disable: false,
-        register: true,
-        scope: '/',
-    },
-    crossOrigin: 'anonymous',
-    webpack: (config, options) =>
-    {
-        config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
-        config.optimization.minimizer.push(new UglifyJsPlugin());
-        return config;
+        dest: 'public'
     }
-});
+})
